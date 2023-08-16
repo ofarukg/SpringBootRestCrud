2ffc9fa17cbf93a565f5fc8fd3e41041fd90cf89
ofarukg <130508202+ofarukg@users.noreply.github.com>
2023-08-16 18:03:42 +0300

    Add files via upload

---
+++ 03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/sprinboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.luv2code.sprinboot.cruddemo.dao;
+
+import com.luv2code.sprinboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    //that's it ... no need to write any code LOLOLOLOLOLOL!!!
+
+
+
+}
